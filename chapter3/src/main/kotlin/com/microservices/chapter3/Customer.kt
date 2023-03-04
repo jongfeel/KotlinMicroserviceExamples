fb99c1495075e62c9cc2a72c9921b79ecf7b5928
jongfeel <DeveloperFEEL@gmail.com>
2023-03-04 12:43:22 +0900

    Add Telephone data class to Customer class and init customer data with telephone

--- chapter3/src/main/kotlin/com/microservices/chapter3/Customer.kt
+++ chapter3/src/main/kotlin/com/microservices/chapter3/Customer.kt
@@ -1,3 +1,5 @@
 package com.microservices.chapter3
 
-data class Customer(var id: Int = 0, var name: String = "")
+data class Customer(var id: Int = 0, var name: String = "", var telephone: Telephone) {
+    data class Telephone(var contryCode: String = "", var telephoneNumber: String = "")
+}
