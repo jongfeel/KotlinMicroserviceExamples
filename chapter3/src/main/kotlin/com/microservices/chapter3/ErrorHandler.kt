7e9b1429541ce6c175075dd04b9a11e90dae1d45
jongfeel <DeveloperFEEL@gmail.com>
2023-03-18 12:04:39 +0900

    Add chapter 3-5 use controller advice and custom response entity

---
+++ chapter3/src/main/kotlin/com/microservices/chapter3/ErrorHandler.kt
@@ -0,0 +1,16 @@
+package com.microservices.chapter3
+
+import com.fasterxml.jackson.core.JsonParseException
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class ErrorHandler {
+    @ExceptionHandler(JsonParseException::class)
+    fun JsonParseExceptionHandler(servletRequest: HttpServletRequest, exception: Exception) : ResponseEntity<ErrorResponse> {
+        return ResponseEntity(ErrorResponse("JSON Error", exception.message ?: "invalid json"), HttpStatus.BAD_REQUEST)
+    }
+}
