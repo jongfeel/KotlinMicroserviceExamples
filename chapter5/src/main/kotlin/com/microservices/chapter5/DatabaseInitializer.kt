f16112ad1ec026b3ff9ba70ece5358c3d564a455
jongfeel <DeveloperFEEL@gmail.com>
2023-05-27 11:02:09 +0900

    Add chapter 5 sprint boot initializer and DatabaseInitializer class

---
+++ chapter5/src/main/kotlin/com/microservices/chapter5/DatabaseInitializer.kt
@@ -0,0 +1,25 @@
+package com.microservices.chapter5
+
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
+import org.springframework.data.mongodb.core.collectionExists
+import org.springframework.stereotype.Component
+
+@Component
+class DatabaseInitializer {
+
+    @Autowired
+    lateinit var mongoOperations: ReactiveMongoOperations
+    @PostConstruct
+    fun initData() {
+        mongoOperations.collectionExists("Customers").subscribe {
+            if (it != true)
+                mongoOperations.createCollection("Customers").subscribe {
+                    println("Customers collections created")
+                }
+            else
+                println("Customers collections already exist")
+        }
+    }
+}
