91409e0ab2caee2cd8e312be7c876edb62850e64
jongfeel <DeveloperFEEL@gmail.com>
2023-05-27 12:06:24 +0900

    Add Customer data class and CustomerRepository interface

--- chapter5/src/main/kotlin/com/microservices/chapter5/CustomerRepository.kt
+++ chapter5/src/main/kotlin/com/microservices/chapter5/CustomerRepository.kt
@@ -1,4 +1,5 @@
 package com.microservices.chapter5
 
-interface CustomerRepository {
-}
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+
+interface CustomerRepository : ReactiveCrudRepository<Customer, Int>
