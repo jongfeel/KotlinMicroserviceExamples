1b81b41f4fc85d8a3efbbbfa2fbf1cc88f52d962
jongfeel <DeveloperFEEL@gmail.com>
2023-07-15 12:19:53 +0900

    Add dependency 'spring-boot-starter-actuator' to microservice

--- chapter6/chapter06-microservice/build.gradle.kts
+++ chapter6/chapter06-microservice/build.gradle.kts
@@ -22,6 +22,7 @@ extra["springCloudVersion"] = "2022.0.3"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
